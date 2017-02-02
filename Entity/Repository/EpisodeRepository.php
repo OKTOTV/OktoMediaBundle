d80033eba01bf66c5dc0a1b9c0202d7ddb3469ef
rs <schmidt91robin@icloud.com>
2017-02-02 14:34:36 +0100

    updated repo call

---
+++ Entity/Repository/EpisodeRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Okto\MediaBundle\Entity\Repository;
+
+use Oktolab\MediaBundle\Entity\Repository\BaseEpisodeRepository;
+
+class EpisodeRepository extends BaseEpisodeRepository
+{
+    public function findAllForClass($episode_class, $query_only = false)
+    {
+        $query = $this->getEntityManager()->createQuery(
+                sprintf("SELECT e, p, s FROM %s e LEFT JOIN e.posterframe p LEFT JOIN e.series s", $episode_class)
+            );
+
+        if ($query_only) {
+            return $query;
+        }
+        return $query->getResult();
+    }
+}
+
+ ?>
