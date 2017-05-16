2cbf62eb09ad24889a7604cf6b489fbb7c88fd9d
rs <schmidt91robin@icloud.com>
2017-05-16 15:16:14 +0200

    default sort by last update

--- Entity/Repository/EpisodeRepository.php
+++ Entity/Repository/EpisodeRepository.php
@@ -9,7 +9,12 @@ class EpisodeRepository extends BaseEpisodeRepository
     public function findAllForClass($episode_class, $query_only = false)
     {
         $query = $this->getEntityManager()->createQuery(
-                sprintf("SELECT e, p, s FROM %s e LEFT JOIN e.posterframe p LEFT JOIN e.series s", $episode_class)
+                sprintf(
+                    "SELECT e, p, s
+                    FROM %s e
+                    LEFT JOIN e.posterframe p
+                    LEFT JOIN e.series s
+                    ORDER BY e.updatedAt DESC", $episode_class)
             );
 
         if ($query_only) {
