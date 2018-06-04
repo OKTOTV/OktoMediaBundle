28315576b2a24aef9e1f7638bc12e7a08097d01a
rs <schmidt91robin@icloud.com>
2018-06-04 10:29:25 +0200

    fixed bug for missing template due to removed bundle inheritance

---
+++ Event/EnqueuedEncodeEpisodeListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Okto\MediaBundle\Event;
+
+use Oktolab\MediaBundle\Event\ImportedEpisodeMetadataEvent;
+use Oktolab\MediaBundle\Event\ImportedEpisodePosterframeEvent;
+
+class EpisodeImportListener {
+
+    private $logbook;
+    private $security_token_storage;
+
+    public function __construct($logbook, $security_token_storage)
+    {
+        $this->logbook = $logbook;
+        $this->security_token_storage = $security_token_storage;
+    }
+
+    public function onEpisodeMetadataImport(ImportedEpisodeMetadataEvent $event)
+    {
+        $username = "system";
+        if ($security_token_storage->getToken()) {
+            $username = $this->security_token_storage->getToken()->getUser()->getUsername();
+        }
+
+        $this->logbook->info(
+            "okto_media.enqueued_encode_episode",
+            ["%user%" => $username],
+            $event->getArgs()['uniqID']
+        );
+    }
+}
